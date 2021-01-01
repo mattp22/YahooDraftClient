919391f574e4d6f276c7102bf6bfc20a300e6c11
Matt <mattp22@vt.edu>
2020-12-31 19:29:42 -0500

    lets try this

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 // set our port
-const port = Process.env.PORT || 3000;
+const port = 3000;
 app.get('/', (req, res) => res.send('Welcome to Tutorialspoint!'));
 
 // startup our app
-app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT || port, ()=> console.log(`Example app listening on port ${port}!`));
